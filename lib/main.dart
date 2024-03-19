35a19245a2e22be236aba5d2e882950986ca11c9
Dagan Larson <lars7283@d.umn.edu>
2024-03-19 14:36:07 -0500

    moved navbar to bottom of screen

--- lib/main.dart
+++ lib/main.dart
@@ -58,24 +58,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return DefaultTabController(length: 4, child: Scaffold(
-      appBar: AppBar(
-        bottom: const TabBar( tabs: [
+    return const DefaultTabController(length: 4, child: Scaffold(
+      bottomNavigationBar: TabBar( tabs: [
           Tab(icon: Icon(Icons.home),),
           Tab(icon: Icon(Icons.settings),),
           Tab(icon: Icon(Icons.person),),
           Tab(icon: Icon(Icons.calendar_month),),
-        ],),
+        ],
       ),
-      body: const TabBarView(
-          children: [
-            Icon(Icons.home),
+      body: TabBarView( children: [
+        Icon(Icons.home),
         Icon(Icons.settings),
         Icon(Icons.person),
         Icon(Icons.calendar_month),
