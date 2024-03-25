ffa333340318bed3c7ef4ad20514826e20f98311
theie006 <theie006@d.umn.edu>
2024-03-25 15:16:29 -0500

    Added in timepicker, very bare bones and still has testing code at the top, but works.

--- lib/logworkout/log_workout.dart
+++ lib/logworkout/log_workout.dart
@@ -1,17 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'package:binarybrigade/workout.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  // Create instances of Workout and Exercise
+  //this is just for testing the file right now:
   Workout workout = Workout('2024-03-18', DateTime.now(), DateTime.now());
   Exercise exercise = Exercise('Running', 10, 100);
 
   // Run the app with LogWorkout widget
-  runApp(MaterialApp(home:LogWorkout(workout: workout, exercise: exercise))
-  );
+  runApp(MaterialApp(home: LogWorkout(workout: workout, exercise: exercise)));
 }
+
 class LogWorkout extends StatelessWidget {
   const LogWorkout({
     Key? key,
@@ -25,87 +24,83 @@ class LogWorkout extends StatelessWidget {
   //logs the workout
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Wrap with Scaffold to provide a material design structure
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Log Workout'),
+          title: Center(child: Text('Log Workout')), // Centering the title
         ),
-        body: ElevatedButton(
+        body: Center(
+          child: ElevatedButton(
             onPressed: () {
               showDialog(
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
-                      title: const Text('Log Workout'),
-                      content: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            //input fields!
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                  labelText: 'Date',
-                                  icon: Icon(Icons.calendar_month)),
-                              initialValue: workout.m_date,
-                              keyboardType: TextInputType.datetime,
-                              onChanged: (value) {
-                                workout.m_date = value;
+                    title: const Text('Log Workout'),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          //input fields!
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Date',
+                                icon: Icon(Icons.calendar_month)),
+                            initialValue: workout.m_date,
+                            keyboardType: TextInputType.datetime,
+                            onChanged: (value) {
+                              workout.m_date = value;
+                            },
+                          ),
+                          _buildTimePicker(
+                            context,
+                            workout.m_timeStart,
+                            'Time Started',
+                                (time) {
+                                workout.m_timeStart = time!;
+                                },
+                          ),
+                          _buildTimePicker(
+                            context,
+                            workout.m_timeEnd,
+                            'Time Ended',
+                              (time) {
+                              workout.m_timeEnd = time!;
                               },
-                            ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    labelText: 'Time Started',
-                    icon: Icon(Icons.watch_later)),
-                keyboardType: TextInputType.datetime,
-                onChanged: (value) {
-                  workout.m_listOfExercises.forEach((exercise) {
-                    workout.m_timeStart = value as DateTime;
-                  });
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    labelText: 'Time Ended',
-                    icon: Icon(Icons.watch_later_outlined)),
-                keyboardType: TextInputType.datetime,
-                onChanged: (value) {
-                  workout.m_listOfExercises.forEach((exercise) {
-                    workout.m_timeEnd = value as DateTime;
-                  });
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    labelText: 'Exercise Type',
-                    icon: Icon(Icons.directions_run)),
-                initialValue: exercise.m_exerciseType,
-                keyboardType: TextInputType.text,
-                onChanged: (value) {
-                  workout.m_listOfExercises.forEach((exercise) {
-                    exercise.m_exerciseType = value;
-                  });
-                },
-              ),
-              // TextFormField(
-              //   decoration: InputDecoration(labelText: 'Exercise Description'),
-              //   keyboardType: TextInputType.text,
-              //   onChanged: (value) {
-              //     workout.m_listOfExercises.forEach((exercise) {
-              //       exercise.m_exerciseDescription = value;
-              //     });
-              //   },
-              // ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Reps'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  workout.m_listOfExercises.forEach((exercise) {
-                    exercise.m_numberOfReps = int.parse(value);
-                  });
-                },
-              ),
-                          ],
-                        ),
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Exercise Type',
+                                icon: Icon(Icons.directions_run)),
+                            initialValue: exercise.m_exerciseType,
+                            keyboardType: TextInputType.text,
+                            onChanged: (value) {
+                              workout.m_listOfExercises.forEach((exercise) {
+                                exercise.m_exerciseType = value;
+                              });
+                            },
+                          ),
+                          // TextFormField(
+                          //   decoration: InputDecoration(labelText: 'Exercise Description'),
+                          //   keyboardType: TextInputType.text,
+                          //   onChanged: (value) {
+                          //     workout.m_listOfExercises.forEach((exercise) {
+                          //       exercise.m_exerciseDescription = value;
+                          //     });
+                          //   },
+                          // ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Reps', icon: Icon(Icons.numbers)),
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) {
+                              workout.m_listOfExercises.forEach((exercise) {
+                                exercise.m_numberOfReps = int.parse(value);
+                              });
+                            },
+                          ),
+                        ],
                       ),
+                    ),
                     actions: <Widget>[
                       TextButton(
                         onPressed: () {
@@ -126,13 +121,47 @@ class LogWorkout extends StatelessWidget {
                 },
               );
             },
-          child: Text('Log Workout'),
-        ),
-    );
+            child: Text('Log Workout'),
+          ),
+        ));
   }
 
   void saveWorkout(Workout workout) {
     //save to database here
     print('Workout logged: $workout');
+  }
+
+  Widget _buildTimePicker(BuildContext context, DateTime initialTime, String label, Function(DateTime?) onChanged) {
+    return GestureDetector(
+      onTap: () async {
+        final pickedTime = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.fromDateTime(initialTime),
+        );
+        if (pickedTime != null) {
+          onChanged(DateTime(
+            0,
+            0,
+            0,
+            pickedTime.hour,
+            pickedTime.minute,
+          ));
+        }
+      },
+      child: AbsorbPointer(
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: label,
+            icon: Icon(Icons.watch_later),
+          ),
+          controller: TextEditingController(
+            text: initialTime != null
+                ? TimeOfDay.fromDateTime(initialTime).format(context)
+                : '',
+          ),
+          keyboardType: TextInputType.datetime,
+        ),
+      ),
+    );
   }
 }
