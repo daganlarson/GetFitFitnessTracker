e82ef0a083e4574a4000b3f6f6e881ed31b520c4
theie006 <theie006@d.umn.edu>
2024-03-25 12:36:45 -0500

    Finally got the emulator to work and the log workout page to actually run

--- lib/logworkout/log_workout.dart
+++ lib/logworkout/log_workout.dart
@@ -3,9 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:binarybrigade/workout.dart';
 import 'package:flutter/services.dart';
 
+void main() {
+  // Create instances of Workout and Exercise
+  Workout workout = Workout('2024-03-18', DateTime.now(), DateTime.now());
+  Exercise exercise = Exercise('Running', 10, 100);
+
+  // Run the app with LogWorkout widget
+  runApp(MaterialApp(home:LogWorkout(workout: workout, exercise: exercise))
+  );
+}
 class LogWorkout extends StatelessWidget {
-  const LogWorkout(
-  {super.key, required this.workout, required this.exercise});
+  const LogWorkout({
+    Key? key,
+    required this.workout,
+    required this.exercise,
+  }) : super(key: key);
 
   final Workout workout;
   final Exercise exercise;
@@ -13,26 +25,36 @@ class LogWorkout extends StatelessWidget {
   //logs the workout
   @override
   Widget build(BuildContext context) {
-    //logging workout UI
-    //why is dart so weird
-    showDialog(context: context, builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Log Workout'),
-        content: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              //input fields!
+    return Scaffold( // Wrap with Scaffold to provide a material design structure
+        appBar: AppBar(
+          title: Text('Log Workout'),
+        ),
+        body: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                      title: const Text('Log Workout'),
+                      content: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            //input fields!
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Date',
+                                  icon: Icon(Icons.calendar_month)),
+                              initialValue: workout.m_date,
+                              keyboardType: TextInputType.datetime,
+                              onChanged: (value) {
+                                workout.m_date = value;
+                              },
+                            ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Date'),
-                initialValue: workout.m_date,
-                keyboardType: TextInputType.datetime,
-                onChanged: (value) {
-                  workout.m_date = value;
-                }
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Time Started'),
+                decoration: const InputDecoration(
+                    labelText: 'Time Started',
+                    icon: Icon(Icons.watch_later)),
                 keyboardType: TextInputType.datetime,
                 onChanged: (value) {
                   workout.m_listOfExercises.forEach((exercise) {
@@ -41,7 +63,9 @@ class LogWorkout extends StatelessWidget {
                 },
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Time Ended'),
+                decoration: const InputDecoration(
+                    labelText: 'Time Ended',
+                    icon: Icon(Icons.watch_later_outlined)),
                 keyboardType: TextInputType.datetime,
                 onChanged: (value) {
                   workout.m_listOfExercises.forEach((exercise) {
@@ -50,7 +74,9 @@ class LogWorkout extends StatelessWidget {
                 },
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Exercise Type'),
+                decoration: const InputDecoration(
+                    labelText: 'Exercise Type',
+                    icon: Icon(Icons.directions_run)),
                 initialValue: exercise.m_exerciseType,
                 keyboardType: TextInputType.text,
                 onChanged: (value) {
@@ -77,45 +103,36 @@ class LogWorkout extends StatelessWidget {
                   });
                 },
               ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Weight'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  workout.m_listOfExercises.forEach((exercise) {
-                    exercise.m_weightUsed = int.parse(value);
-                  });
+                          ],
+                        ),
+                      ),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          // Save workout
+                          saveWorkout(workout);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Save'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Cancel'),
+                      )
+                    ],
+                  );
                 },
-              ),
-            ],
-          ),
+              );
+            },
+          child: Text('Log Workout'),
         ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              //Save workout
-              saveWorkout(workout);
-              Navigator.of(context).pop();
-            },
-            child: const Text('Save'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('Cancel'),
-          )
-          ]
-      );
-    },
     );
-    throw UnimplementedError();
   }
 
   void saveWorkout(Workout workout) {
     //save to database here
-
     print('Workout logged: $workout');
   }
 }
-
-
