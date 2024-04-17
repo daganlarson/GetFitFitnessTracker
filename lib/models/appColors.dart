c3c4b37106bd69916b6781603405910782f03074
Christopher Engstrom <engst340@umn.edu>
2024-04-17 10:48:38 -0500

    created app theme and app color classes

---
+++ lib/models/appColors.dart
@@ -0,0 +1,15 @@
+
+import 'dart:ui';
+
+class AppColors{
+
+  final paleLime = const Color(0xffd5ff5f);
+  final cottonWhite = const Color(0xfff3f3f3);
+  final cottonGrey = const Color(0xffbababa);
+  final raisinBlack = const Color(0xff1e1e25);
+  final midGrey = const Color(0xff656566);
+  final outerSpace = const Color(0xff2d2d36);
+
+  const AppColors();
+
+}
---
+++ lib/models/appTheme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'appColors.dart';
+
+class AppTheme {
+
+  static const colors = AppColors();
+
+  const AppTheme._();
+  static ThemeData define() {
+    return ThemeData(
+      fontFamily: ""
+    )
+  }
+}
