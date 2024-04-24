6632c3c1cf428ec4e44c65a94daf0fbb92a88fcf
Christopher Engstrom <engst340@umn.edu>
2024-04-24 00:23:58 -0500

    adding assests to pubspec.yaml and cleaning up benefits page

--- lib/views/benefits_page.dart
+++ lib/views/benefits_page.dart
@@ -6,7 +6,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 import '../models/appColors.dart';
 import '../models/appTheme.dart';
 
-final List<String> pngEntries = <String>['assets/images/FitnessGraphic.png', 'assets/images/runningGraphic.png', 'yogaGraphic.jpg'];
+final List<String> pngEntries = <String>['assets/images/FitnessGraphic.png','assets/images/runningGraphic.png','assets/images/yogaGraphic.jpg'];
 final List<String> titleEntries = <String>['The Benefits of Exercise!', '11 Powerful Reasons Why You Should Be Running', 'Yoga for Mindfulness'];
 final List<String> subtitleEntries = <String>['12 exercise benefits to encourage a workout routine', 'The Incredible Benefits of Running', 'The Benefits of a Daily Yoga Pracitce'];
 final List<String> addressURLs = <String>['https://www.betterup.com/blog/exercise-benefits', 'https://www.beachbodyondemand.com/blog/benefits-of-running', 'https://mplsheart.org/news/yoga-and-meditation-study-communities-impacted-heart-disparities?gad_source=1&gclid=CjwKCAjwuJ2xBhA3EiwAMVjkVMWXHFK23j1eBuekW5WdLLhtj7511Cmi9UxPAWA1vDqJgddPZN3EgBoCUZgQAvD_BwE'];
